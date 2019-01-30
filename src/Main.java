15d5826222909e1b5febc035a131d4349828b4c7
Sandrine <sandrine.yven@gmail.com>
2019-01-30 11:46:28 -0500

    exemple mongoDB

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.Arrays;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class Main {
+	 
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MongoClient mongoClient = new MongoClient();
+		
+		 Document doc = new Document("name", "MongoDB")
+	                .append("type", "database")
+	                .append("count", 1)
+	                .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
+	                .append("info", new Document("x", 203).append("y", 102));
+		
+		
+		MongoDatabase database = mongoClient.getDatabase("Exemple");
+		MongoCollection<Document> collection = database.getCollection("test");
+		collection.insertOne(doc);
+		
 	}
 
 }
